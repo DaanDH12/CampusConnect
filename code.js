e0c7788afdbb6478555b7f867cf4aae2c2d49d9d
DaanDH12 <daanhoning@kpnmail.nl>
2023-05-23 21:50:10 +0200

    New update pushed

---
+++ code.js
@@ -0,0 +1,30 @@
+const card = document.querySelectorAll('.memory-card');
+const imageContainer = document.querySelector('.tv-screen');
+const image = document.querySelector('.tv-screen-one');
+var memoryCards = document.querySelectorAll('.memory-card');
+const totalImages = 3;
+let currentImageIndex = 1;
+
+
+
+
+card.forEach(card => {
+	card.addEventListener('click', event => {
+		console.log('click!')
+	})
+})
+
+imageContainer.addEventListener('click', () => {
+  currentImageIndex++;
+  if (currentImageIndex > totalImages) {
+    currentImageIndex = 1;
+  }
+  const nextImageSrc = `tv-img/${currentImageIndex}.png`;
+  image.src = nextImageSrc;
+});
+
+memoryCards.forEach(function(card) {
+  card.addEventListener('click', function() {
+    card.classList.toggle('flip');
+  });
+});
